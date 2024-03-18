d5d2f729081d62e0165f7f4a0e5e9132678bf52c
Mariia Okunieva <okunieva.masha@gmail.com>
2024-03-18 19:05:45 +0100

    Initial project

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AverageCalculator
+{
+    class AvarageCalculator
+    {
+        public static double CalculateAverage(int[] array)
+        {
+            if (array == null || array.Length == 0)
+            {
+                throw new ArgumentException("Array cannot be null or empty", nameof(array));
+            }
+
+            int sum = 0;
+            foreach (int num in array)
+            {
+                sum += num;
+            }
+            return (double)sum / array.Length;
+        }
+    }
+}
