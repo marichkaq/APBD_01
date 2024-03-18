797981fc7c34b4de41fa9cdedf30a00209204d56
Mariia Okunieva <s28299@pjwstk.edu.pl>
2024-03-18 19:20:32 +0100

    Modification 1

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
+        public static int FindMaxValue(int[] array)
+        {
+            if (array == null || array.Length == 0)
+            {
+                throw new ArgumentException("Array cannot be null or empty.", nameof(array));
+            }
 
-Console.WriteLine("Hello, World!");
+            int max = array[0];
+            foreach (int value in array)
+            {
+                if (value > max)
+                {
+                    max = value; 
+                }
+            }
+            return max;
+        }
+    }
