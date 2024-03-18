3ee31f11a5dad73f7ed20d7fd29ab3952c621c09
Mariia Okunieva <s28299@pjwstk.edu.pl>
2024-03-18 19:28:58 +0100

    feature-new commit

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@
                 throw new ArgumentException("Array cannot be null or empty.", nameof(array));
             }
 
-            int max = array[0];
+            double max = array[0];
             foreach (int value in array)
             {
                 if (value > max)
