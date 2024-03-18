53205373bc32d0bec7bb9a8edc9ddd83db254d71
Mariia Okunieva <okunieva.masha@gmail.com>
2024-03-18 19:19:11 +0100

    Initial commit

--- Program.cs
+++ Program.cs
@@ -1,30 +1,3 @@
-﻿using System;
+﻿// See https://aka.ms/new-console-template for more information
 
-namespace AverageCalculator
-{
-    class AvarageCalculator
-    {
-       static void Main(string[] args)
-        {
-            int[] numbers = { 1, 2, 3, 4, 5 };
-            
-            double average = CalculateAverage(numbers);
-            
-            Console.WriteLine($"The average is: {average}");
-        }
-        public static double CalculateAverage(int[] array)
-        {
-            if (array == null || array.Length == 0)
-            {
-                throw new ArgumentException("Array cannot be null or empty", nameof(array));
-            }
-
-            int sum = 0;
-            foreach (int num in array)
-            {
-                sum += num;
-            }
-            return (double)sum / array.Length;
-        }
-    }
-}
+Console.WriteLine("Hello, World!");
