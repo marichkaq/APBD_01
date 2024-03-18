a60a4ddd0945de58f50beb9815ab9c6c81ba3598
Mariia Okunieva <okunieva.masha@gmail.com>
2024-03-18 18:42:04 +0100

    Initial project

---
+++ APBD_01/APBD_01/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace PalindromeChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a word to check if it's a palindrome: ");
+            string input = Console.ReadLine();
+            
+        }
+        
+    }
+}
